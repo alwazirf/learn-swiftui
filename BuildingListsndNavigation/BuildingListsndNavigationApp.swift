e28f4d62e9a5d9edf9da173c5bf3bec4e044743d
Alwazir Fitrah <Alwazir.Fitrah@maybank.co.id>
2024-11-23 21:18:36 +0700

    Initial Commit

---
+++ BuildingListsndNavigation/BuildingListsndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingListsndNavigationApp.swift
+//  BuildingListsndNavigation
+//
+//  Created by Alwazir Fitrah on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BuildingListsndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
